1416818f66296c1eba84542e522957038ff72c7a
NguyenCaoVanHUS <nguyencaovan.k56@gmail.com>
2023-04-25 16:08:19 +0700

    update webgis

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
-// const path = require('path')
-const express = require('express');
-const morgan = require('morgan');
-// const handlebars = require('express-handlebars');
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+
 const app = express();
 const port = 3001;
 
+app.use(express.static('src/public'));
 app.use(morgan('combined'));
 
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-// app.set('view', path.join(__dirname, 'resources/views'));
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', 'src/resources/views');
 
 app.get('/', (req, res) => {
-    res.send('home')
+    res.render('home')
+});
+
+app.get('/news', (req, res) => {
+    res.render('news')
 });
 
 app.listen(port, () => {
