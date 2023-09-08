ee7be808676d6fbe9bf53ca5fe4c2d8c604114ff
NguyenCaoVanHUS <nguyencaovan.k56@gmail.com>
2023-09-08 08:43:41 +0700

    them babel node

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import morgan from 'morgan';
 import { engine } from 'express-handlebars';
 import route from './routes/index.js';
 import db from './config/db/mongodb.js';
+import path from 'path';
 
 db.connect();
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.static('src/public'));
 app.use(morgan('combined'));
@@ -23,3 +24,4 @@ route(app);
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 });
+
